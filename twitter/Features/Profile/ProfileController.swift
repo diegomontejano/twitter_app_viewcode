7b6826d9927a6489d1b6ef6d305acac1b789e3b9
Diego Montejano <diegomontejano@Diegos-MacBook-Air.local>
2022-09-12 09:18:57 -0300

    configure ProfileController

--- twitter/Features/Profile/ProfileController.swift
+++ twitter/Features/Profile/ProfileController.swift
@@ -14,6 +14,7 @@ class ProfileController: UITableViewController, ConfigureView {
     func viewSettings() {
         navigationItem.title = "Profile"
         view.backgroundColor = .systemPink
+        tableView.register(TweetCell.self, forCellReuseIdentifier: TweetCell.identifier)
     }
     
     func viewHierarchy() {
@@ -22,6 +23,33 @@ class ProfileController: UITableViewController, ConfigureView {
     
     
     // MARK: - Methods
+    
+        
+}
+
+
+// MARK: - Extension
+extension ProfileController {
+    // configure number of cells
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    // configure cell as TweetCell()
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TweetCell.identifier, for: indexPath) as! TweetCell
+        return cell
+    }
+    
+    // configure cell height
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 500
+    }
+    
     
     
 }
